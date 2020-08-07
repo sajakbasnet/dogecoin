24d5efa4de7239c22a25f3817ddf4ca7cbc213fc
Ek-macmini <ek-macmini@Ek-macminis-Mac-mini.local>
2020-08-07 08:48:09 +0545

    minor changes

--- app/Http/Controllers/Api/auth/customAccessTokenController.php
+++ app/Http/Controllers/Api/auth/customAccessTokenController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers\Api\auth;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
-use Illuminate\Http\Request;
 use Laravel\Passport\Http\Controllers\AccessTokenController;
 use Psr\Http\Message\ServerRequestInterface;
 
