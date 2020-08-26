e44666603a333407909441695ceab81a68e9b249
Ek-macmini <ek-macmini@Ek-macminis-Mac-mini.local>
2020-08-26 08:44:31 +0545

    sidebar js minor chnages

--- resources/asset/system/js/backend.js
+++ resources/asset/system/js/backend.js
@@ -1,75 +1,82 @@
 $(document).ready(function () {
-    sidebar.init()
-    confirmDelete.init()
-    fileInput.init()
-  })
-  
-  const sidebar = (function () {
-    const init = () => {
-      highlightModule()
+  sidebar.init()
+  confirmDelete.init()
+  fileInput.init()
+})
+
+const sidebar = (function () {
+  const init = () => {
+    highlightModule()
+  }
+
+  const highlightModule = () => {
+    const $navSidebar = $('.nav-sidebar')
+
+    var path = window.location.pathname.split('/');
+    lastSegment = path[path.length - 1];
+    if (lastSegment == 'create') {
+      path = path[path.length - 2];
+    } else if (lastSegment == 'edit') {
+      path = path[path.length - 3];
+    } else {
+      path = lastSegment;
     }
-  
-    const highlightModule = () => {
-      const $navSidebar = $('.nav-sidebar')
-      const path = window.location.pathname.split('/')
-      const module = path[2]
-      if (module) {
-        $navSidebar.find('a[href$=\'' + module + '\']').closest('li').addClass('active')
-        $navSidebar.find('a[href$=\'' + module + '\']').parents().eq(2).addClass('active')
-        $navSidebar.find('a[href$=\'' + module + '\']').closest('.collapse').collapse('show')
+    if (path !== undefined) {
+      $navSidebar.find("a[href$='" + path + "']").closest('li').addClass('active');
+      $navSidebar.find("a[href$='" + path + "']").parents().eq(2).addClass('active');
+      $navSidebar.find("a[href$='" + path + "']").closest('.collapse').collapse();
+    }
+  }
+
+  return {
+    init,
+  }
+})()
+
+const confirmDelete = (function () {
+  const $modal = $('#confirmDeleteModal')
+  const $deleteBtn = $('.btn-delete')
+  const $deleteForm = $modal.find('form')
+
+  const init = () => {
+    attachEventListeners()
+  }
+
+  const attachEventListeners = () => {
+    $deleteBtn.on('click', handleDeleteBtnClick)
+    $modal.on('hidden.bs.modal', handleModalHidden)
+  }
+
+  const handleDeleteBtnClick = function () {
+    const url = $(this).data('href')
+    setDeleteUrl(`${url}?_method=DELETE`)
+  }
+
+  const handleModalHidden = function () {
+    setDeleteUrl('')
+  }
+
+  const setDeleteUrl = url => {
+    $deleteForm.attr('action', url)
+  }
+
+  return {
+    init,
+  }
+})()
+
+const fileInput = (function () {
+  const init = function () {
+    $(document).on('change', '.custom-file-input', function () {
+      let files = []
+      for (let i = 0; i < $(this)[0].files.length; i++) {
+        files.push($(this)[0].files[i].name)
       }
-    }
-  
-    return {
-      init,
-    }
-  })()
-  
-  const confirmDelete = (function () {
-    const $modal = $('#confirmDeleteModal')
-    const $deleteBtn = $('.btn-delete')
-    const $deleteForm = $modal.find('form')
-  
-    const init = () => {
-      attachEventListeners()
-    }
-  
-    const attachEventListeners = () => {
-      $deleteBtn.on('click', handleDeleteBtnClick)
-      $modal.on('hidden.bs.modal', handleModalHidden)
-    }
-  
-    const handleDeleteBtnClick = function () {
-      const url = $(this).data('href')
-      setDeleteUrl(`${url}?_method=DELETE`)
-    }
-  
-    const handleModalHidden = function () {
-      setDeleteUrl('')
-    }
-  
-    const setDeleteUrl = url => {
-      $deleteForm.attr('action', url)
-    }
-  
-    return {
-      init,
-    }
-  })()
-  
-  const fileInput = (function () {
-    const init = function () {
-      $(document).on('change', '.custom-file-input', function () {
-        let files = []
-        for (let i = 0; i < $(this)[0].files.length; i++) {
-          files.push($(this)[0].files[i].name)
-        }
-        $(this).next('.custom-file-label').html(files.join(', '))
-      })
-    }
-  
-    return {
-      init,
-    }
-  })()
-  
+      $(this).next('.custom-file-label').html(files.join(', '))
+    })
+  }
+
+  return {
+    init,
+  }
+})()
