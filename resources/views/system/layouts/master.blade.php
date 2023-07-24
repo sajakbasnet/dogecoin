ae4c7a124f676ac56b712e4f13a583011598fd8f
Sujan Ghorasaine <sujan.ghorasaine@ekbana.info>
2023-07-24 10:12:24 +0545

    fix: template design in translation and config page

--- resources/views/system/layouts/master.blade.php
+++ resources/views/system/layouts/master.blade.php
@@ -27,15 +27,13 @@
                             </div>
                             <div class="col-12 col-sm-6">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"> <a class="home-item" href="index.html"><i
-                                                data-feather="home"></i></a>
-                                    </li>
                                     <li class="breadcrumb-item"> Dashboard</li>
                                     <li class="breadcrumb-item active"> {{ $title ?? ''}}</li>
                                 </ol>
                             </div>
                         </div>
                     </div>
+                        @yield('heading-contents')
                 </div>
                 @yield('contents')
             </div>
