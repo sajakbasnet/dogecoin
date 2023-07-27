7899142c5f20e1679e900f7efc93d88ef743eb43
Sajak Basnet <sajak.basnet@ekbana.info>
2023-07-27 14:16:09 +0545

    fix: email template delte issue resolved

--- app/Interfaces/System/EmailInterface.php
+++ app/Interfaces/System/EmailInterface.php
@@ -5,9 +5,10 @@ namespace App\Interfaces\System;
 interface EmailInterface
 {
     public function getAllData($data, $selectedColumns = [], $pagination = true);
-    
-    public function create($data);    
+
+    public function create($data);
 
     public function update($id, $data);
-  
+
+    public function delete($request, $id);
 }
