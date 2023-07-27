88c755029f6353c8803697e148ab81d77e9451a8
Sajak Basnet <sajak.basnet@ekbana.info>
2023-07-27 14:35:08 +0545

    feat: changed config service pattern to repository pattern

--- app/Services/System/ConfigService.php
+++ app/Services/System/ConfigService.php
@@ -4,6 +4,7 @@ namespace App\Services\System;
 
 use App\Exceptions\NotDeletableException;
 use App\Model\Config;
+use App\Repositories\System\ConfigRepository;
 use App\Services\Service;
 use App\Traits\ImageTrait;
 
@@ -13,26 +14,14 @@ class ConfigService extends Service
 
     public $dir = '/uploads/config';
 
-    public function __construct(Config $config)
+    public function __construct(ConfigRepository $configRepository)
     {
-        parent::__construct($config);
+        $this->configRepository = $configRepository;
     }
 
     public function getAllData($data, $selectedColumns = [], $pagination = true)
     {
-        $query = $this->query();
-
-        if (isset($data->keyword) && $data->keyword !== null) {
-            $query->where('label', 'LIKE', '%' . $data->keyword . '%');
-        }
-        if (count($selectedColumns) > 0) {
-            $query->select($selectedColumns);
-        }
-        if ($pagination) {
-            return $query->orderBy('id', 'ASC')->paginate(PAGINATE);
-        }
-
-        return $query->orderBy('id', 'ASC')->get();
+        return $this->configRepository->getAllData($data);
     }
 
     //config type key value pair
@@ -48,43 +37,44 @@ class ConfigService extends Service
 
     public function indexPageData($request)
     {
-        return ['items' => $this->getAllData($request), 'types' => $this->configTypeOptions()];
+        return [
+            'items' => $this->getAllData($request),
+            'types' => $this->configTypeOptions()
+        ];
     }
 
     public function store($request)
-    {
-        $data = $request->except('_token');
+    {   
+        $data = $request->except('_token');        
         if (strtolower($request->type) == 'file') {
             $data['value'] = $this->uploadImage($this->dir, 'value');
         }
-
-        return $this->model->create($data);
+        return $this->configRepository->create($data);
     }
 
     public function update($request, $id)
     {
         $data = $request->except('_token');
-        $config = $this->itemByIdentifier($id);
+        $config = $this->configRepository->itemByIdentifier($id);
         if (strtolower($config->type) == 'file') {
             $this->removeImage($this->dir, $config->value);
             $data['value'] = $this->uploadImage($this->dir, 'value');
         }
-        $config->update($data);
+        $this->configRepository->update($config, $data);
         setConfigCookie();
 
-        return $config = $this->itemByIdentifier($id);
+        return $config =  $this->configRepository->itemByIdentifier($id);
     }
 
     public function delete($request, $id)
     {
-        $config = $this->itemByIdentifier($id);
+        $config =  $this->configRepository->itemByIdentifier($id);
         if (in_array($id, [1, 2, 3])) {
             throw new NotDeletableException;
         }
         if (strtolower($config->type) == 'file') {
             $this->removeImage($this->dir, $config->value);
         }
-
-        return $config->delete();
+        return $this->configRepository->delete($config, $id);
     }
 }
